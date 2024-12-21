2770db015bdb7db8c7d1661a4870fc90b3be237f
Vinit Jahagirdar <115438154+Vinit1909@users.noreply.github.com>
2024-12-20 19:38:46 -0800

    update and delete documents

--- convex/documents.ts
+++ convex/documents.ts
@@ -6,7 +6,6 @@ export const create = mutation({
     args: { title: v.optional(v.string()), initialContent: v.optional(v.string()) },
     handler: async (ctx, args) => {
         const user = await ctx.auth.getUserIdentity();
-
         if (!user) {
             throw new ConvexError("Unauthorized");
         }
@@ -23,5 +22,49 @@ export const get = query({
     args: { paginationOpts: paginationOptsValidator },
     handler: async (ctx, args) => {
         return await ctx.db.query("documents").paginate(args.paginationOpts);
+    },
+});
+
+export const removeById = mutation({
+    args: { id: v.id("documents") },
+    handler: async (ctx, args) => {
+        const user = await ctx.auth.getUserIdentity();
+        if (!user) {
+            throw new ConvexError("Unauthorized");
+        }
+
+        const document = await ctx.db.get(args.id);
+        if (!document) {
+            throw new ConvexError("Document not found!")
+        }
+
+        const isOwner = document.ownerId === user.subject;
+        if (!isOwner) {
+            throw new ConvexError("Unauthorized");
+        }
+
+        return await ctx.db.delete(args.id);
+    },
+});
+
+export const updateById = mutation({
+    args: { id: v.id("documents"), title: v.string() },
+    handler: async (ctx, args) => {
+        const user = await ctx.auth.getUserIdentity();
+        if (!user) {
+            throw new ConvexError("Unauthorized");
+        }
+
+        const document = await ctx.db.get(args.id);
+        if (!document) {
+            throw new ConvexError("Document not found!")
+        }
+
+        const isOwner = document.ownerId === user.subject;
+        if (!isOwner) {
+            throw new ConvexError("Unauthorized");
+        }
+
+        return await ctx.db.patch(args.id, { title: args.title });
     },
 });
