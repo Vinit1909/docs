7c6afa5ff388cf1bf37e56e0bd80ccf97d7d2f9f
Vinit Jahagirdar <115438154+Vinit1909@users.noreply.github.com>
2024-12-22 19:33:00 -0800

    populate user info

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -16,6 +16,7 @@ import {
     MenubarTrigger,
 } from "@/components/ui/menubar";
 
+import { Avatars } from "./avatars";
 import { DocumentInput } from "./document-input";
 import { BoldIcon, FileIcon, FileJsonIcon, FilePenIcon, FilePlusIcon, FileTextIcon, GlobeIcon, ItalicIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, StrikethroughIcon, TextIcon, TrashIcon, UnderlineIcon, Undo2Icon } from "lucide-react";
 import { BsFilePdf } from "react-icons/bs";
@@ -209,6 +210,7 @@ export const Navbar = () => {
                 </div>
             </div>
             <div className="flex gap-3 items-center pl-6">
+                <Avatars />
                 <OrganizationSwitcher
                     afterCreateOrganizationUrl="/"
                     afterLeaveOrganizationUrl="/"
